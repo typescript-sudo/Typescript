1e79d9c8ae128a7cc7994044e1aa30dee122169f
typescript-sudo <typescript@myyahoo.com>
2026-02-10 18:24:27 +0800

    Create 01_primitive.ts

---
+++ 01_primitive.ts
@@ -0,0 +1,18 @@
+"use strict";
+// string
+let username = "sangam";
+let age = 21;
+let isCreator = true;
+
+
+const big = 2n ** 63n - 1n;
+// const mixed = big + age;
+const TOKEN = Symbol('TOKEN');
+console.log(TOKEN);
+function yearsToDay(years) {
+    return years * 365;
+}
+
+
+console.log(yearsToDay(5));
+console.log(username.toUpperCase());
